fc1767b35af72cef5d46974dfebb36d12d2ac105
karlosvas <126987511+karlosvas@users.noreply.github.com>
2024-12-08 10:46:02 +0100

    2024 08-la-carrera-de-los-renos

---
+++ 2024-🎄/08-la-carrera-de-renos.ts
@@ -0,0 +1,57 @@
+function drawRace(indices: number[], length: number): string {
+  let pista: string = "";
+
+  let j: number = 0;
+  for (let i of indices) {
+    let espacios: number = indices.length - j - 1;
+    let actual: string[] = (" ".repeat(espacios) + "~".repeat(length)).split("");
+
+    if (i > 0) actual[espacios + i] = "r";
+    else if (i < 0) actual[i + espacios + length] = "r";
+
+    pista += actual.join("") + " /" + (j + 1);
+    if (j != indices.length - 1) pista += "\n";
+    j++;
+  }
+
+  return pista;
+}
+
+/*
+¡Es hora de seleccionar a los renos más rápidos para los viajes de Santa! 🦌🎄
+Santa Claus ha organizado unas emocionantes carreras de renos para decidir cuáles están en mejor forma.
+
+Tu tarea es mostrar el progreso de cada reno en una pista de nieve en formato isométrico.
+
+La información que recibes:
+
+indices: Un array de enteros que representan el progreso de cada reno en la pista:
+0: El carril está vacío.
+Número positivo: La posición actual del reno desde el inicio de la pista.
+Número negativo: La posición actual del reno desde el final de la pista.
+length: La longitud de cada carril.
+Devuelve un string que represente la pista de la carrera:
+
+Cada carril tiene exactamente length posiciones llenas de nieve (~).
+Cada reno se representa con la letra r.
+Los carriles están numerados al final con /1, /2, etc.
+La vista es isométrica, por lo que los carriles inferiores están desplazados hacia la derecha.
+*/
+
+/*
+Ejemplos: drawRace([0, 5, -3], 10);
+  ~~~~~~~~~~ /1
+ ~~~~~r~~~~ /2
+~~~~~~~r~~ /3
+
+drawRace([2, -1, 0, 5], 8);
+   ~~r~~~~~ /1
+  ~~~~~~~r /2
+ ~~~~~~~~ /3
+~~~~~r~~ /4
+
+drawRace([3, 7, -2], 12);
+  ~~~r~~~~~~~~ /1
+ ~~~~~~~~r~~~ /2
+~~~~~~~~~r~~ /3
+*/
